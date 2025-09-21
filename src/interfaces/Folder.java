4eda123f3a00157d4771d42dba3281d3f76d1f65
Primuu <trentowski.adam@gmail.com>
2025-09-21 16:21:31 +0200

    Add the basis of the task

---
+++ src/interfaces/Folder.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface Folder {
+    String getName();
+    String getSize();
+}
---
+++ src/interfaces/MultiFolder.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+import java.util.List;
+
+public interface MultiFolder extends Folder {
+    List<Folder> getFolders();
+}
