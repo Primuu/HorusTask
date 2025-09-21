5eccb2744424939e082476d8d1a5a8f43d9717ec
Primuu <trentowski.adam@gmail.com>
2025-09-21 17:08:56 +0200

    Add the solution to the task (with taking into account MultiFolder)

--- src/FileCabinet.java
+++ src/FileCabinet.java
@@ -1,41 +1,65 @@
 import interfaces.Cabinet;
 import interfaces.Folder;
+import interfaces.MultiFolder;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class FileCabinet implements Cabinet {
     private List<Folder> folders;
+    // 1. Jeśli getSize()/getName() może zwrócić null, należałoby użyć bezpieczniejszego porównania,
+    // które nie rzuci NullPointerException
 
-    // zwraca dowolny element o podanej nazwie
     @Override
     public Optional<Folder> findFolderByName(String name) {
-        if (name == null) throw new IllegalArgumentException("Invalid name.");
+        // findFolderByName wskazuje na unikalność nazw folderów
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("Invalid name.");
 
-        // 1. findFolderByName wskazuje na unikalność nazw folderów
-        // 2. jeśli nazwa folderu może być nullem, należałoby użyć bezpieczniejszego porównania,
-        // które nie rzuci NullPointerException
-        return folders.stream()
+        return flattenFolders().stream()
                 .filter(f -> f.getName().equals(name))
                 .findFirst();
     }
 
-    // zwraca wszystkie foldery podanego rozmiaru SMALL/MEDIUM/LARGE
-    // * sygnatura funkcji wskazuje na String, jendak rozważyłbym użycie Enum
     @Override
     public List<Folder> findFoldersBySize(String size) {
+        // sygnatura funkcji wskazuje na String, jendak rozważyłbym użycie Enum
         if (size == null || !size.equals(Size.SIZE_SMALL)
             && !size.equals(Size.SIZE_MEDIUM)
             && !size.equals(Size.SIZE_LARGE)) throw new IllegalArgumentException("Invalid size.");
 
-        return folders.stream()
+        return flattenFolders().stream()
                 .filter(f -> f.getSize().equals(size))
                 .toList();
     }
 
-    //zwraca liczbę wszystkich obiektów tworzących strukturę
     @Override
     public int count() {
-        return (folders != null) ? folders.size() : 0;
+        return flattenFolders().size();
+    }
+
+    private List<Folder> flattenFolders() {
+        // w zależności od rozmairu listy folders, można rozważyć cacheowanie spłaszczonej listy,
+        // żeby za każdym razem nie budować jej od nowa
+        List<Folder> flatten = new ArrayList<>();
+        if (folders == null) return flatten;
+
+        for (Folder folder: folders) {
+            depthFirstSearch(folder, flatten);
+        }
+        return flatten;
+    }
+
+    private void depthFirstSearch(Folder folder, List<Folder> flatten) {
+        if (folder == null) return;
+        flatten.add(folder);
+
+        // zakłada się że folder może być subfolderem tylko jednego folderu,
+        // w przeciwnym razie należałoby śledzić odwiedzone foldery
+        if (folder instanceof MultiFolder multiFolder && multiFolder.getFolders() != null) {
+            for (Folder subFolder: multiFolder.getFolders()) {
+                depthFirstSearch(subFolder, flatten);
+            }
+        }
     }
 }
